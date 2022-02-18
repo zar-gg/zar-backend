1ec47cbde4621cb0463819226dbd1c87e01214c4
ali2faraz <63406806+ali2faraz@users.noreply.github.com>
2022-02-18 20:10:06 +0500

    a

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ app = Flask(__name__)
 @app.route("/")
 def hello_patient0():
     return "Hello Patient 0 you fkin noob. Dont Try HArd fk u"
+@app.route("/faraz")
+def hello_patient01():
+    return "Hello Patient 0 you fkin noob. Dont Try HArd fk u"
+
 
 if __name__ == '__main__':
     app.run("0.0.0.0",port=8000)
